8f1cd684892b243a6b6e8db104cbc80b7521cd95
Lukasz Grzegorzewski <grzegorzewski.luk@gmail.com>
2022-11-07 00:15:06 +0100

    finis exercise MySQL-POST_user

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
-const express = require("express");
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
 
+require("dotenv").config();
+const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5001;
 
@@ -18,6 +20,9 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
